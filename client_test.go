7f78f961187022663450341ec08313636ac9bd4c
George Ornbo <george@shapeshed.com>
2016-11-22 11:57:44 +0000

    Add test for makeURL

--- client_test.go
+++ client_test.go
@@ -6,7 +6,25 @@ func TestLatLongToString(t *testing.T) {
 	s := latLongToString(52.847875, -0.664398)
 	expected := "52.847875,-0.664398"
 	if s != expected {
-		t.Errorf("expected string %q; got %q", expected, s)
+		t.Errorf("expected %q; got %q", expected, s)
+	}
+
+}
+
+func TestMakeURL(t *testing.T) {
+	params := RequestParams{
+		Key:       "17b1e8cae7b654290659b438557def7e",
+		Latitude:  52.847875,
+		Longitude: -0.664397,
+		Units:     "si",
+	}
+	s, err := makeURL(&params)
+	if err != nil {
+		t.Errorf("got err: %v", err)
+	}
+	expected := "https://api.darksky.net/forecast/17b1e8cae7b654290659b438557def7e/52.847875,-0.664397?units=si"
+	if s != expected {
+		t.Errorf("expected %q; got %q", expected, s)
 	}
 
 }
