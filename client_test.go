7abbeb57d281c88d9cb3381c8a64cec3763fd849
George Ornbo <george@shapeshed.com>
2016-11-23 10:26:08 +0000

    Add table tests for makeURL

--- client_test.go
+++ client_test.go
@@ -11,20 +11,72 @@ func TestLatLongToString(t *testing.T) {
 
 }
 
+type makeURLTest struct {
+	params   RequestParams
+	expected string
+}
+
+var makeURLTests = []makeURLTest{
+	{
+		RequestParams{
+			Key:       "17b1e8cae7b654290659b438557def7e",
+			Latitude:  52.847875,
+			Longitude: -0.664397,
+			Exclude:   "currently,minutely",
+		},
+		"https://api.darksky.net/forecast/17b1e8cae7b654290659b438557def7e/52.847875,-0.664397?exclude=currently%2Cminutely",
+	},
+	{
+		RequestParams{
+			Key:       "17b1e8cae7b654290659b438557def7e",
+			Latitude:  52.847875,
+			Longitude: -0.664397,
+			Extend:    "hourly",
+		},
+		"https://api.darksky.net/forecast/17b1e8cae7b654290659b438557def7e/52.847875,-0.664397?extend=hourly",
+	},
+	{
+		RequestParams{
+			Key:       "17b1e8cae7b654290659b438557def7e",
+			Latitude:  52.847875,
+			Longitude: -0.664397,
+			Lang:      "fr",
+		},
+		"https://api.darksky.net/forecast/17b1e8cae7b654290659b438557def7e/52.847875,-0.664397?lang=fr",
+	},
+	{
+		RequestParams{
+			Key:       "17b1e8cae7b654290659b438557def7e",
+			Latitude:  52.847875,
+			Longitude: -0.664397,
+			Units:     "si",
+		},
+		"https://api.darksky.net/forecast/17b1e8cae7b654290659b438557def7e/52.847875,-0.664397?units=si",
+	},
+	{
+		RequestParams{
+			Key:       "17b1e8cae7b654290659b438557def7e",
+			Latitude:  52.847875,
+			Longitude: -0.664397,
+			Exclude:   "currently,minutely",
+			Extend:    "hourly",
+			Lang:      "fr",
+			Units:     "si",
+		},
+		"https://api.darksky.net/forecast/17b1e8cae7b654290659b438557def7e/52.847875,-0.664397?exclude=currently%2Cminutely&extend=hourly&lang=fr&units=si",
+	},
+}
+
 func TestMakeURL(t *testing.T) {
-	params := RequestParams{
-		Key:       "17b1e8cae7b654290659b438557def7e",
-		Latitude:  52.847875,
-		Longitude: -0.664397,
-		Units:     "si",
-	}
-	s, err := makeURL(&params)
-	if err != nil {
-		t.Errorf("got err: %v", err)
-	}
-	expected := "https://api.darksky.net/forecast/17b1e8cae7b654290659b438557def7e/52.847875,-0.664397?units=si"
-	if s != expected {
-		t.Errorf("expected %q; got %q", expected, s)
+
+	for _, tt := range makeURLTests {
+		actual, err := makeURL(&tt.params)
+		if err != nil {
+			t.Errorf("got err: %v", err)
+		}
+		if actual != tt.expected {
+			t.Errorf("\nexpected %v\n actual %v", tt.expected, actual)
+		}
 	}
 
 }
